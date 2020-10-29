9e71293ff4f2d2717e99a20ab5245847362f4420
Alessandro Astone <ales.astone@gmail.com>
2020-10-29 08:40:54 +0100

    a52q: Define a soong namespace
    Change-Id: Ida759ee1786c667e766da29103f0ecb54950c104

--- device.mk
+++ device.mk
@@ -35,5 +35,9 @@ PRODUCT_COPY_FILES += \
 # Camera (id 54 is macro)
 $(call soong_config_set,samsungCameraVars,extra_ids,54)
 
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(DEVICE_PATH)
+
 # call the extra setup
 $(call inherit-product, vendor/extra/product.mk)
