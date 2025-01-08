79b13f3a6a8d21a44c82f166a117ca98b713d11d
koko-07870 <ko282ko82@gmail.com>
2025-01-08 14:39:07 +0530

    a52q: Update from A525FXXSAFXJ2
    Signed-off-by: koko-07870 <ko282ko82@gmail.com>

--- aosp_a52q.mk
+++ aosp_a52q.mk
@@ -44,8 +44,8 @@ PRODUCT_GMS_CLIENTID_BASE := android-samsung
 PRODUCT_SHIPPING_API_LEVEL := 30
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="a52qnsxx-user 11 RP1A.200720.012 A525FXXS9FXJ1 release-keys" \
-    BuildFingerprint=samsung/a52qnsxx/a52q:11/RP1A.200720.012/A525FXXS9FXJ1:user/release-keys
+    BuildDesc="a52qnsxx-user 11 RP1A.200720.012 A525FXXSAFXJ2 release-keys" \
+    BuildFingerprint=samsung/a52qnsxx/a52q:11/RP1A.200720.012/A525FXXSAFXJ2:user/release-keys
 
 # Security patch
-VENDOR_SECURITY_PATCH := 2024-10-01
+VENDOR_SECURITY_PATCH := 2024-11-01
